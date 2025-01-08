175c494686f0fd1323ec95650af1e73a9fcf53da
Ashish <ashuusheoran@gmail.com>
2025-01-08 22:56:35 +0530

    app constant core screen added

---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppConstant{
+
+  static String appMainName = 'Shop Mate';
+  static String appPoweredBy = 'Powered By ASHUU';
+  static const appMainColor = Color(0xFFbf1b08);
+  static const appScendoryColor = Color(0xFF981206);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+}
